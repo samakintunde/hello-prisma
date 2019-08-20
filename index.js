fc4c9239626a611815e1105cd83cdfdeb9cf03b1
Samuel Akintunde <samakintunde37@gmail.com>
2019-08-20 12:31:17 +0100

    Completed step 2

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const { prisma } = require("./generated/prisma-client");
 
 async function main() {
-  const newUser = await prisma.createUser({ name: "Doug" });
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+  // const newUser = await prisma.createUser({
+  //   name: "Eva",
+  //   email: "eva@prisma.io",
+  //   posts: {
+  //     create: [
+  //       {
+  //         title: "First post"
+  //       },
+  //       {
+  //         title: "Second post"
+  //       }
+  //     ]
+  //   }
+  // });
+  // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+  // const allUsers = await prisma.users();
+  // console.log(allUsers);
+  // const allPosts = await prisma.posts();
+  // console.log(allPosts);
 
-  const allUsers = await prisma.users();
-  console.log(allUsers);
+  // Query Relational Data
+  const postsByUser = await prisma
+    .user({
+      email: "eva@prisma.io"
+    })
+    .posts();
+
+  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`);
 }
 
 main().catch(e => console.error(e));

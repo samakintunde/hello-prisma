bb91cf39004501eff4cf85db0c6fe09176663392
Samuel Akintunde <samakintunde37@gmail.com>
2019-08-20 13:21:14 +0100

    Completed GraphQL API

--- index.js
+++ index.js
@@ -1,34 +1,46 @@
 const { prisma } = require("./generated/prisma-client");
+const { GraphQLServer } = require("graphql-yoga");
+const resolvers = require("./resolvers/index");
 
-async function main() {
-  // const newUser = await prisma.createUser({
-  //   name: "Eva",
-  //   email: "eva@prisma.io",
-  //   posts: {
-  //     create: [
-  //       {
-  //         title: "First post"
-  //       },
-  //       {
-  //         title: "Second post"
-  //       }
-  //     ]
-  //   }
-  // });
-  // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
-  // const allUsers = await prisma.users();
-  // console.log(allUsers);
-  // const allPosts = await prisma.posts();
-  // console.log(allPosts);
+// async function main() {
+//   // const newUser = await prisma.createUser({
+//   //   name: "Eva",
+//   //   email: "eva@prisma.io",
+//   //   posts: {
+//   //     create: [
+//   //       {
+//   //         title: "First post"
+//   //       },
+//   //       {
+//   //         title: "Second post"
+//   //       }
+//   //     ]
+//   //   }
+//   // });
+//   // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+//   // const allUsers = await prisma.users();
+//   // console.log(allUsers);
+//   // const allPosts = await prisma.posts();
+//   // console.log(allPosts);
 
-  // Query Relational Data
-  const postsByUser = await prisma
-    .user({
-      email: "eva@prisma.io"
-    })
-    .posts();
+//   // Query Relational Data
+//   const postsByUser = await prisma
+//     .user({
+//       email: "eva@prisma.io"
+//     })
+//     .posts();
 
-  console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`);
-}
+//   console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`);
+// }
 
-main().catch(e => console.error(e));
+// main().catch(e => console.error(e));
+
+const server = new GraphQLServer({
+  typeDefs: "./schemas/schema.graphql",
+  resolvers,
+  context: {
+    prisma
+  }
+});
+
+server.start(() => console.log("Server started!"));
